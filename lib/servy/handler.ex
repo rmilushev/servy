6e549d3c27df8edafae1b9214c16d09d1c4426ff
Rumen Milushev <r@vert.bg>
2018-06-30 16:46:42 +0300

    Refactor route function with pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -37,27 +37,23 @@ defmodule Servy.Handler do
        status: nil }
   end
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
-
-  def route(conv, "GET", "/wildthings") do
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Mecho1, Mecan2, Pticho" }
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{ method: "GET", path: "/bears" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(conv, "DELETE", "/bears/" <> _id) do
+  def route(%{ method: "DELETE", path: "/bears" <> _id } = conv) do
     %{ conv | status: 403, resp_body: "Cannot delete bears!!!" }
   end
 
-  def route(conv, _method, path) do
+  def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here..." }
   end
 
